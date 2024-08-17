5cbd39decba06d3bceb540e658702644a511beaf
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-08-17 22:03:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var firstName = document.querySelector('input[name="First Name:"]').value;
+    var lastName = document.querySelector('input[name="Last Name:"]').value;
+    var phoneNo = document.querySelector('input[name="phone no:"]').value;
+    var emailId = document.querySelector('input[name="email id:"]').value;
+
+    alert(`First Name: ${firstName} \nLast Name: ${lastName} \nPhone Number: ${phoneNo} \nEmail ID: ${emailId}`);
+});
