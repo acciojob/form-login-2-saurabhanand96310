4ea8d9334377a46e0f770185759745fb5100a265
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-09-20 21:07:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-cy.wait()
-document.querySelector('form').addEventListener('submit', function(event) {
+
+const form =document.querySelector('form')
+	form.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    var firstName = document.querySelector('input[name="First Name"]').value;
-    var lastName = document.querySelector('input[name="Last Name"]').value;
-    var phoneNo = document.querySelector('input[name="Phone Number"]').value;
-    var emailId = document.querySelector('input[name="Email ID"]').value;
+    var firstName = document.querySelector('#name').value;
+    var lastName = document.querySelector('#Last').value;
+    var phoneNo = document.querySelector('#ph').value;
+    var emailId = document.querySelector('#id').value;
 
     alert(`First Name: ${firstName} \nLast Name: ${lastName} \nPhone Number: ${phoneNo} \nEmail ID: ${emailId}`);
 });
